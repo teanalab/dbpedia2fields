3f9cbb37623c4562aff042af35303546df061a7f
Fedor Nikolaev <fsqcds@gmail.com>
2016-10-07 22:48:00 -0400

    Trying to reduce index size by filtering out entities without rdfs:comment or rdfs:label predicates and using only one name for relatedentitynames and incomingentitynames

--- src/main/scala/edu/wayne/dbpedia2fields/TriplesToTrec.scala
+++ src/main/scala/edu/wayne/dbpedia2fields/TriplesToTrec.scala
@@ -41,6 +41,10 @@ object TriplesToTrec {
 
   val wikiLinkTextPredicate = "<http://dbpedia.org/ontology/wikiPageWikiLinkText>"
 
+  val rdfsCommentPredicate = "<http://www.w3.org/2000/01/rdf-schema#comment>"
+
+  val rdfsLabelPredicate = "<http://www.w3.org/2000/01/rdf-schema#label>"
+
   def main(args: Array[String]): Unit = {
     val pathToTriples = args(0)
     val pathToOutput = args(1)
@@ -57,6 +61,8 @@ object TriplesToTrec {
     }.map { case (subj, pred, obj) =>
       (subj, obj)
     }.distinct().cache()
+
+    val namesCompact = names.groupByKey().map { case (subj, names) => (subj, names.head)}
 
     val categories = triples.filter { case (subj, pred, obj) =>
       pred == subjectPredicate
@@ -95,7 +101,7 @@ object TriplesToTrec {
       obj.startsWith("<")
     }.map { case (subj, pred, obj) =>
       (obj, (predNamesMap.value.get(pred), subj))
-    }.join(names).map { case (obj, ((predName, subj), objName)) =>
+    }.join(namesCompact).map { case (obj, ((predName, subj), objName)) =>
       (subj, (predName, objName))
     }.union(filenames).distinct()
 
@@ -105,7 +111,7 @@ object TriplesToTrec {
       obj.startsWith("<")
     }.map { case (subj, pred, obj) =>
       (subj, (predNamesMap.value.get(pred), obj))
-    }.join(names).map { case (subj, ((predName, obj), subjName)) =>
+    }.join(namesCompact).map { case (subj, ((predName, obj), subjName)) =>
       (obj, (subjName, predName))
     }.distinct()
 
@@ -125,52 +131,61 @@ object TriplesToTrec {
       }
     ).distinct()
 
-    new CoGroupedRDD(Seq(names, attributes, categories, similarEntityNames, relatedEntityNames, incomingEntityNames),
-      Partitioner.defaultPartitioner(names, attributes, categories, similarEntityNames, relatedEntityNames, incomingEntityNames)).
+    val indexedEntities = triples.filter { case (subj, pred, obj) =>
+      pred == rdfsCommentPredicate || pred == rdfsLabelPredicate
+    }.map { case (subj, pred, obj) =>
+      (subj, true)
+    }.distinct()
+
+    new CoGroupedRDD(Seq(names, attributes, categories, similarEntityNames, relatedEntityNames, incomingEntityNames, indexedEntities),
+      Partitioner.defaultPartitioner(names, attributes, categories, similarEntityNames, relatedEntityNames, incomingEntityNames, indexedEntities)).
       mapValues { case Array(namesArray, attributesArray, categoriesArray, similarEntityNamesArray,
-      relatedEntityNamesArray, incomingEntityNamesArray) =>
+      relatedEntityNamesArray, incomingEntityNamesArray, indexedEntitiesArray) =>
         (namesArray.asInstanceOf[Seq[String]],
           attributesArray.asInstanceOf[Seq[(Option[String], String)]],
           categoriesArray.asInstanceOf[Seq[String]],
           similarEntityNamesArray.asInstanceOf[Seq[String]],
           relatedEntityNamesArray.asInstanceOf[Seq[(Option[String], String)]],
-          incomingEntityNamesArray.asInstanceOf[Seq[(String, Option[String])]])
+          incomingEntityNamesArray.asInstanceOf[Seq[(String, Option[String])]],
+          indexedEntitiesArray.asInstanceOf[Seq[Boolean]])
       }.flatMap { case (entityUri, (namesSeq, attributesSeq, categoriesSeq, similarEntityNamesSeq,
-    relatedEntityNamesSeq, incomingEntityNamesSeq)) =>
-      Array("<DOC>\n<DOCNO>" + entityUri + "</DOCNO>\n<TEXT>") ++
-        (if (namesSeq.nonEmpty) Array("<names>") ++
-          namesSeq.map(extractLiteralText).distinct ++
-          Array("</names>")
-        else Seq()) ++
-        (if (attributesSeq.nonEmpty) Array("<attributes>") ++
-          attributesSeq.map {
-            case (Some(predName), obj) => extractLiteralText(predName) + " " + extractLiteralText(obj)
-            case (None, obj) => extractLiteralText(obj)
-          }.distinct ++ Array("</attributes>")
-        else Seq()) ++
-        (if (categoriesSeq.nonEmpty) Array("<categories>") ++
-          categoriesSeq.map(extractLiteralText).distinct ++
-          Array("</categories>")
-        else Seq()) ++
-        (if (similarEntityNamesSeq.nonEmpty) Array("<similarentitynames>") ++
-          similarEntityNamesSeq.map(extractLiteralText).distinct ++
-          Array("</similarentitynames>")
-        else Seq()) ++
-        (if (relatedEntityNamesSeq.nonEmpty) Array("<relatedentitynames>") ++
-          relatedEntityNamesSeq.map {
-            case (Some(predName), objName) => extractLiteralText(predName) + " " + extractLiteralText(objName)
-            case (None, objName) => extractLiteralText(objName)
-          }.distinct ++
-          Array("</relatedentitynames>")
-        else Seq()) ++
-        (if (incomingEntityNamesSeq.nonEmpty) Array("<incomingentitynames>") ++
-          incomingEntityNamesSeq.map {
-            case (subjName, Some(predName)) => extractLiteralText(subjName) + " " + extractLiteralText(predName)
-            case (subjName, None) => extractLiteralText(subjName)
-          }.distinct ++
-          Array("</incomingentitynames>")
-        else Seq()) ++
-        Array("</TEXT>\n</DOC>")
+    relatedEntityNamesSeq, incomingEntityNamesSeq, indexedEntitiesSeq)) =>
+      if (indexedEntitiesSeq.nonEmpty)
+        Array("<DOC>\n<DOCNO>" + entityUri + "</DOCNO>\n<TEXT>") ++
+          (if (namesSeq.nonEmpty) Array("<names>") ++
+            namesSeq.map(extractLiteralText).distinct ++
+            Array("</names>")
+          else Seq()) ++
+          (if (attributesSeq.nonEmpty) Array("<attributes>") ++
+            attributesSeq.map {
+              case (Some(predName), obj) => extractLiteralText(predName) + " " + extractLiteralText(obj)
+              case (None, obj) => extractLiteralText(obj)
+            }.distinct ++ Array("</attributes>")
+          else Seq()) ++
+          (if (categoriesSeq.nonEmpty) Array("<categories>") ++
+            categoriesSeq.map(extractLiteralText).distinct ++
+            Array("</categories>")
+          else Seq()) ++
+          (if (similarEntityNamesSeq.nonEmpty) Array("<similarentitynames>") ++
+            similarEntityNamesSeq.map(extractLiteralText).distinct ++
+            Array("</similarentitynames>")
+          else Seq()) ++
+          (if (relatedEntityNamesSeq.nonEmpty) Array("<relatedentitynames>") ++
+            relatedEntityNamesSeq.map {
+              case (Some(predName), objName) => extractLiteralText(predName) + " " + extractLiteralText(objName)
+              case (None, objName) => extractLiteralText(objName)
+            }.distinct ++
+            Array("</relatedentitynames>")
+          else Seq()) ++
+          (if (incomingEntityNamesSeq.nonEmpty) Array("<incomingentitynames>") ++
+            incomingEntityNamesSeq.map {
+              case (subjName, Some(predName)) => extractLiteralText(subjName) + " " + extractLiteralText(predName)
+              case (subjName, None) => extractLiteralText(subjName)
+            }.distinct ++
+            Array("</incomingentitynames>")
+          else Seq()) ++
+          Array("</TEXT>\n</DOC>")
+      else Array[String]()
     }.saveAsTextFile(pathToOutput)
   }
 }
